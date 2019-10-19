aecb2433409f78968adefff0da17b98fb880ebcf
Tom König <hi@tomknig.de>
2019-10-19 17:47:21 +0200

    Add allowed cors methods

--- goat-herd/x/cardservice/client/rest/rest.go
+++ goat-herd/x/cardservice/client/rest/rest.go
@@ -23,6 +23,7 @@ const (
 func corsMiddleware(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			  w.Header().Set("Access-Control-Allow-Origin", "*")
+			  w.Header().Set("Access-Control-Allow-Methods", "GET,OPTIONS,PUT,POSTS,DELETE")
     })
 }
 
