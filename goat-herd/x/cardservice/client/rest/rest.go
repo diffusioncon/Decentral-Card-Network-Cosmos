954520a313d2d9614b17c88ff57482fd9cf333fb
Tom König <hi@tomknig.de>
2019-10-19 18:16:44 +0200

    Set ac allow headers

--- goat-herd/x/cardservice/client/rest/rest.go
+++ goat-herd/x/cardservice/client/rest/rest.go
@@ -24,6 +24,8 @@ func corsMiddleware(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			  w.Header().Set("Access-Control-Allow-Origin", "*")
 			  w.Header().Set("Access-Control-Allow-Methods", "GET,OPTIONS,PUT,POST,DELETE")
+			  w.Header().Set("Access-Control-Allow-Headers", "*")
+			  w.Header().Set("Access-Control-Max-Age", "86400")
     })
 }
 
