8e7266779450a4ee6ab52d2c5f7c1dc1f24001f0
Tom König <hi@tomknig.de>
2019-10-20 11:48:00 +0200

    Let rest server know where the blockchain live

--- goat-herd/scripts/startRESTserver.sh
+++ goat-herd/scripts/startRESTserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cscli rest-server --chain-id testCardchain --trust-node --laddr tcp://0.0.0.0:1317
+cscli rest-server --chain-id testCardchain --trust-node --laddr tcp://0.0.0.0:1317 --node tcp://blockchain:26657
