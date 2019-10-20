cece01819e475244a647fa867368f00d86ea964c
Tom König <hi@tomknig.de>
2019-10-20 08:43:24 +0200

    Scrape all pages and write names to file

--- shepherd/scrape-magic-card-names.rb
+++ shepherd/scrape-magic-card-names.rb
@@ -1,12 +1,21 @@
 require 'wombat'
 
-(1..3).to_a.each do |page| # 683
-  Wombat.crawl do
-    base_url "https://deckbox.org/games/mtg/cards?p=#{page}"
-    path "/games/mtg/cards"
+names = []
 
-    cards do
-      card_names({ css: ".card_name a" }, :list)
-    end
+(1..683).to_a.each do |page|
+  result = Wombat.crawl do
+    base_url "https://deckbox.org"
+    path "/games/mtg/cards?p=#{page}"
+
+
+    card_names({ css: "td.card_name" }, :list)
+  end
+
+  names << result["card_names"]
+end
+
+File.open("names.csv", "w+") do |f|
+  names.each do |name|
+    f.puts(name)
   end
 end
